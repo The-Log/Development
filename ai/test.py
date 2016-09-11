0050da24dc752b8e10d85cb4be774d5821fe3f7e
Ankur Mishra <2018ankurm@gmail.com>
2016-09-11 17:23:31 -0400

    made puzzle class, yet to make solver

--- ai/test.py
+++ ai/test.py
@@ -1,16 +1,44 @@
-class gayness:
-    def __init__(self, name):
-        if(name == 'Kyle' or name == "Kyle Gerner" or name == 'Kylie' or name == 'Kylie Gerner'):
-            self.homosexuality = True
-        else:
-            self.homosexuality = False
-    def is_suicidal(self):
-        if(self.homosexuality == True):
-            print('im truly gay')
-        else:
-            print('commit suduko')
-    def gay(dick):
-        print('i am gay')
-x = gayness('Kylie')
-x.gay()
-list = []
+import numpy as np
+
+class sliding_puzzle():
+    def __init__(self):
+        self.puzzle = np.array([[2,1,4],[5,6,8],[4,7,0]])
+        self.empty_row = 2
+        self.empty_col = 2
+    def shift_up(self):
+        temp = self.puzzle[self.empty_row][self.empty_col]
+        self.puzzle[self.empty_row][self.empty_col] = self.puzzle[self.empty_row - 1][self.empty_col]
+        self.puzzle[self.empty_row - 1][self.empty_col] = temp
+        self.empty_row = self.empty_row - 1
+        print("Empty Spot Moved Up:")
+        sp.display()
+    def shift_down(self):
+        temp = self.puzzle[self.empty_row][self.empty_col]
+        self.puzzle[self.empty_row][self.empty_col] = self.puzzle[self.empty_row + 1][self.empty_col]
+        self.puzzle[self.empty_row + 1][self.empty_col] = temp
+        self.empty_row = self.empty_row + 1
+        print("Empty Spot Moved Down:")
+        sp.display()
+    def shift_right(self):
+        temp = self.puzzle[self.empty_row][self.empty_col]
+        self.puzzle[self.empty_row][self.empty_col] = self.puzzle[self.empty_row][self.empty_col + 1]
+        self.puzzle[self.empty_row][self.empty_col + 1] = temp
+        self.empty_col = self.empty_col + 1
+        print("Empty Spot Moved Right:")
+        sp.display()
+    def shift_left(self):
+        temp = self.puzzle[self.empty_row][self.empty_col]
+        self.puzzle[self.empty_row][self.empty_col] = self.puzzle[self.empty_row][self.empty_col - 1]
+        self.puzzle[self.empty_row][self.empty_col - 1] = temp
+        self.empty_col = self.empty_col - 1
+        print("Empty Spot Moved Left:")
+        sp.display()
+    def display(self):
+        print(self.puzzle)
+        print("")
+
+sp = sliding_puzzle()
+print("Original:")
+sp.display()
+sp.shift_up()
+sp.shift_left()
