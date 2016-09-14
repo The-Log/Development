48de2c362147846dae818301e7a9b6c9200559b9
Ankur Mishra <2018ankurm@gmail.com>
2016-09-13 23:41:15 -0400

    Lesson 1 of C++ Book Complete

---
+++ cv/basics/1.2.hello/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+//
+// Created by Ankur Mishra on 9/13/16.
+//
+int main(){
+
+    //Exercise 1.3
+    /*std ::cout <<"Hello World!"<< std::endl;
+    return 0;
+     */
+
+    //Exercise 1.4
+    std::cout<<"Enter 2 Numbers"<<std::endl;
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout<<"The product of " << v1 << " and " << v2 << " is " << v1 * v2 <<std::endl;
+    return 0;
+
+    //Exercise 1.5
+    /*std::cout<<"Enter 2 Numbers"<<std::endl;
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout<<"The product of ";
+    std::cout<< v1 ;
+    std::cout<< " and " ;
+    std::cout<< v2 ;
+    std::cout<< " is " ;
+    std::cout<< v1 * v2 ;
+    std::cout<<std::endl;
+     */
+
+    //Exercise 1.6
+    //Not Legal, as it needs std::cout before the second and third lines to run properly.
+    return 0;
+};
