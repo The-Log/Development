e78b39ef9964fa52393bd6f45dd810c10f256f61
Ankur Mishra <2018ankurm@gmail.com>
2016-09-30 23:36:52 -0400

    Makes pixel cooirdinates

--- cv/labs/main.cpp
+++ cv/labs/main.cpp
@@ -6,17 +6,29 @@
 #include <vector>
 
 using namespace std;
+Matrix multPointMatrix(Matrix in, Matrix M){
+  Matrix out = Matrix(1, 4);
+  out(0,0)  = in(0,0) * M(0,0) + in(0,1) * M(1,0) + in(0,2) * M(2,0) +  M(3,0);
+  out(0,1)  = in(0,0) * M(0,1) + in(0,1) * M(1,1) + in(0,2) * M(2,1) +  M(3,1);
+  out(0,2)  = in(0,0) * M(0,2) + in(0,1) * M(1,2) + in(0,2) * M(2,2) +  M(3,2);
+  out(0,3)  = in(0,0) * M(0,3) + in(0,1) * M(1,3) + in(0,2) * M(2,3) +  M(3,3);
+  float w = out(0,3);
+  out(0,0) /= w;
+  out(0,1) /= w;
+  out(0,2) /= w;
 
+  return out;
+}
 Matrix setProjectionMatrix(const float &near, const float &far, Matrix m)
 {
-    float scale = 1 / tan(90 * 0.5 * 3.14 / 180);
-    m(0,0) = scale;
-    m(1,1) = scale;
-    m(2,2) = -far / (far - near);
-    m(3,2) = -far * near / (far - near);
-    m(2,3) = -1;
-    m(3,3) = 0;
-    return m;
+  float scale = 1 / tan(90 * 0.5 * M_PI / 180);
+  m(0,0) = scale;
+  m(1,1) = scale;
+  m(2,2) = -far / (far - near);
+  m(3,2) = -far * near / (far - near);
+  m(2,3) = -1;
+  m(3,3) = 0;
+  return m;
 }
 Matrix setCamera(Matrix m)
 {
@@ -31,8 +43,8 @@ Matrix setCamera(Matrix m)
 }
 
 int main() {
-  int imageWidth = 640;
-  int imageHeight = 480;
+  int imageWidth = 500;
+  int imageHeight = 500;
   string line;
   ifstream myFile ("/Users/ankurM/Development/cv/labs/cube.txt");
   getline (myFile,line);
@@ -49,17 +61,17 @@ int main() {
   {
     while (getline (myFile,line) )
     {
-        Matrix m = Matrix(1, 4);
-        int a, b, c;
-        istringstream iss(line);
-        iss >> a >> b >> c;
-        m(0,j) = a;
-        m(0,j+1) =b;
-        m(0,j+2) =c;
-        m(0,j+3) = 1;
-        //m.display();
-        arr.push_back(m);
-        ++i;
+      Matrix m = Matrix(1, 4);
+      int a, b, c;
+      istringstream iss(line);
+      iss >> a >> b >> c;
+      m(0,j) = a;
+      m(0,j+1) =b;
+      m(0,j+2) =c;
+      m(0,j+3) = 1;
+      //m.display();
+      arr.push_back(m);
+      ++i;
     }
     myFile.close();
   }
@@ -67,13 +79,16 @@ int main() {
     cout << "Unable to open file";
     return 0;
   }
+  vector<int> xco;
+  vector<int> yco;
   for (unsigned i = 0; i<arr.size(); i++){
-     arr.at(i) *= shift;
-     arr.at(i) *= pm;
-     Matrix temp = arr.at(i);
-     double projX = ((temp(0,0) + 1) * 0.5) * imageWidth;
-     double projY = (1 - ((temp(0,1) + 1) * 0.5)) * imageHeight;
-     cout << projX << ", " << projY << endl;
+    Matrix temp = multPointMatrix(arr.at(i) , shift);
+    temp = multPointMatrix(temp , pm);
+    double projX = (temp(0,0) + 1) * 0.5 * imageWidth;
+    double projY = (1 - ((temp(0,1) + 1) * 0.5)) * imageHeight;
+    xco.push_back(projX);
+    yco.push_back(projY);
+    std::cout << projX << ", "<< projY << std::endl;
   }
 
   return 0;
