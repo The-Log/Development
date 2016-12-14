4eed2c02c3e257afa490f7b93b4b08ad12d795ca
Ankur Mishra <2018ankurm@gmail.com>
2016-12-14 13:58:58 -0500

    Optimized

--- ai/tictactoe/strategy.py
+++ ai/tictactoe/strategy.py
@@ -6,8 +6,9 @@ MAX = "X"
 MIN = "O"
 TIE = "TIE"
 INF = math.inf
+DICT = dict()
 
-def minimax_strategy():
+def minimax_strategy(maxd):
     def strategy(board, player):
         return minimax(board, player)
     return strategy
@@ -42,8 +43,15 @@ def max_dfs(board, player):
   v = -INF
   move = -1
   for m in c.actions(board):
-      new_value = min_dfs(c.make_move(board, player, m), c.toggle(player))[0]
+      new_board = c.make_move(board, player, m)
+      if (new_board, player) in DICT:
+          new_value = DICT[(new_board, player)]
+      else:
+          new_value = min_dfs(new_board, c.toggle(player))[0]
+          DICT[(new_board, player)] = new_value
       if new_value > v:
+          if new_value == 1:
+              return new_value, m
           v = new_value
           move = m
   return v, move
@@ -58,8 +66,15 @@ def min_dfs(board, player):
   v = INF
   move = -1
   for m in c.actions(board):
-      new_value = max_dfs(c.make_move(board, player, m), c.toggle(player))[0]
+      new_board = c.make_move(board, player, m)
+      if (new_board, player) in DICT:
+          new_value = DICT[(new_board, player)]
+      else:
+          new_value = max_dfs(new_board, c.toggle(player))[0]
+          DICT[(new_board, player)] = new_value
       if new_value < v:
+          if new_value == -1:
+              return new_value, m
           v = new_value
           move = m
   return v, move
