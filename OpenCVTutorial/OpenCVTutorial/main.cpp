e90439ef56d732c40bf38e8387b4c73b72f51aa2
Ankur Mishra <2018ankurm@gmail.com>
2016-12-23 18:11:34 -0500

    Revert "Moved OpenCVTutorial"
    This reverts commit 629b25680349481ba2561c9204142f50d3da89a4.

---
+++ OpenCVTutorial/OpenCVTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTutorial
+//
+//  Created by Ankur Mishra on 12/20/16.
+//  Copyright © 2016 Ankur Mishra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
