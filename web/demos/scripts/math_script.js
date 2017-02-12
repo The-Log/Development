e60ba330ce3b75ede9029b09f29cf201f43aa2d8
the-log <2018ankurm@gmail.com>
2017-02-12 16:40:08 -0500

    Demos for in class tutorials

---
+++ web/demos/scripts/math_script.js
@@ -0,0 +1,9 @@
+function clicked() {
+  console.log("Button was clicked");
+  var first = document.getElementById("first");
+  var second = document.getElementById("second");
+  var v1 = first.value;
+  var v2 = second.value;
+   var product = v1 * v2;
+  document.getElementById("outputDiv").innerHTML = product;
+}
