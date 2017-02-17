8ec38b55d3e0eb53d930045bd7bf9683716c5f43
the-log <2018ankurm@gmail.com>
2017-02-17 14:07:16 -0500

    Element Creation Demo + First Lab Complete

--- web/labs/scripts/quiz.js
+++ web/labs/scripts/quiz.js
@@ -1,31 +1,82 @@
-time = 120;
-function timer()
+time = 240;
+var score = 0;
+var isRunning = false;
+function disableText(){
+  document.getElementById("input").style.visibility = "hidden";
+}
+function startTimer(){
+  if(isRunning == false){
+    counter = setInterval(decrementTime, 1000);
+    document.getElementById("input").style.visibility = "visible";
+    isRunning = true;
+  }
+}
+function decrementTime()
 {
   time = time-1;
   if (time <= 0)
   {
-     alert('Game over');
+     alert('Game over. You got: ' + score + "/" + richPeople.length);
      reset();
      return;
   }
   document.getElementById("timer").innerHTML= time + " secs";
 }
 
-function reset() {
-  time = 120;
+function pause() {
   clearTimeout(counter);
+  isRunning = false;
+  document.getElementById("input").style.visibility = "hidden";
+}
+
+function reset() {
+  time = 240;
+  score = 0;
+  clearTimeout(counter);
+  document.getElementById("input").style.visibility = "hidden";
   document.getElementById("timer").innerHTML= time + " secs";
+  isRunning = false;
+  for (var i = 0; i < richPeople.length; i++) {
+    document.getElementById("rich" + [i+1]).innerHTML= "";
+  }
+}
+
+function check(){
+  var in1 = document.getElementById("input");
+  var i;
+  for(i = 0; i < richPeople.length; i++){
+    if (richPeople[i].toLowerCase() == in1.value.toLowerCase()){
+      document.getElementById("rich" + [i+1]).innerHTML= richPeople[i];
+      document.getElementById("input").value = "";
+      score =  score + 1;
+      if(score == richPeople.length){
+        alert("You won!");
+        reset();
+      }
+    }
+    console.log(score);
+  }
 }
 
 var richPeople = [
   "Bill Gates",
   "Amancio Ortega",
+  "Jeff Bezos",
+  "Vladimir Putin",
   "Warren Buffet",
   "Carlos Slim",
-  "Jeff Bezos",
   "Mark Zuckerberg",
+  "Ingvar Kamprad",
   "Larry Ellison",
-  "Micheal Bloomberg",
   "Charles Koch",
-  "David Koch"
-]
+  "David Koch",
+  "Larry Page",
+  "Sergey Brin",
+  "Micheal Bloomberg",
+  "Liliane Bettencourt",
+  "Bernard Arnault",
+  "Christy Walton",
+  "Sam Walton",
+  "Jim Walton",
+  "King of Thailand"
+];
