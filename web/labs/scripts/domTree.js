9ac1b84905656bdcaa2ae6961822e273645b78ab
the-log <2018ankurm@gmail.com>
2017-02-21 22:39:15 -0500

    Started Lab02, minor edits

---
+++ web/labs/scripts/domTree.js
@@ -0,0 +1,11 @@
+function dfs() {
+  var frontier = [];
+  //frontier.push(start_node);
+  while (true) {
+    if(frontier.length == 0)
+      return null;
+    current = frontier.pop();
+    //goal_test()
+    //frontier.push(get_children);
+  }
+}
