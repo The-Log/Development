42357752d57c2dac6ec38ddc60d14c4d76779680
the-log <2018ankurm@gmail.com>
2017-02-25 10:15:31 -0500

    Finished Lab02. However, wish to make the divs programatically.

---
+++ web/demos/scripts/domTree.js
@@ -0,0 +1,5 @@
+function clickFunction(event) {
+  console.log(event);
+  var tgt = event.target;
+  console.log(tgt);
+}
--- web/labs/scripts/domTree.js
+++ web/labs/scripts/domTree.js
@@ -1,11 +1,24 @@
-function dfs() {
-  var frontier = [];
-  //frontier.push(start_node);
-  while (true) {
-    if(frontier.length == 0)
-      return null;
-    current = frontier.pop();
-    //goal_test()
-    //frontier.push(get_children);
+var isRunning = false;
+var idnum = 0;
+function highlight(event){
+  var tgtid = event.target.id;
+  console.log(idnum);
+  document.getElementById(idnum).style.border = "";
+  idnum = tgtid - 1;
+  if(isRunning == false){
+    console.log(idnum);
+    setInterval(call, 1000);
+    isRunning = true;
+  }
+}
+function call() {
+  if (idnum >= 0 && idnum <= 14)
+    document.getElementById(idnum).style.border = "";
+  if(idnum >= 14){
+    console.log("waiting for next click");
+  }
+  else {
+    idnum ++;
+    document.getElementById(idnum).style.border = "thick solid yellow";
   }
 }
