6021129333fc72f3eb195ddc30494b0533227dc3
the-log <2018ankurm@gmail.com>
2017-02-26 00:41:52 -0500

    Refined Lab02. Still wish to add edit

--- web/labs/scripts/domTree.js
+++ web/labs/scripts/domTree.js
@@ -3,18 +3,23 @@ var idnum = 0;
 function highlight(event){
   var tgtid = event.target.id;
   console.log(idnum);
-  document.getElementById(idnum).style.border = "";
+  console.log(tgtid);
+  if(tgtid == "bs-example-navbar-collapse-1")
+    tgtid = 0
+  else{
+    document.getElementById(idnum).style.border = "";
+  }
   idnum = tgtid - 1;
   if(isRunning == false){
     console.log(idnum);
-    setInterval(call, 1000);
+    setInterval(call, 700);
     isRunning = true;
   }
 }
 function call() {
-  if (idnum >= 0 && idnum <= 14)
+  if (idnum >= 0 && idnum <= 15)
     document.getElementById(idnum).style.border = "";
-  if(idnum >= 14){
+  if(idnum >= 15){
     console.log("waiting for next click");
   }
   else {
