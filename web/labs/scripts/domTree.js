56dcb5bab63e9dae4048e2ac0e058f1584835dcd
the-log <2018ankurm@gmail.com>
2017-03-13 15:34:56 -0400

    Lab03 Stuff + Updates in Lab02

--- web/labs/scripts/domTree.js
+++ web/labs/scripts/domTree.js
@@ -21,23 +21,22 @@ function highlight2(event){
   if(isRunning == false){
     isRunning = true;
     clearTimeout(counter);
-    counter = setInterval(call2, 700);
+    counter = setInterval(dfs, 700);
     return;
   }
 }
 
-function call2(){
+function dfs(){
   console.log(l);
   var element = l.pop();
-  if(target.parentElement.parentElement.nextElementSibling != null && l.indexOf(target.parentElement.parentElement.nextElementSibling ) <= -1 && temp == 0){
+  if(target.parentElement.parentElement.nextElementSibling != null && l.indexOf(target.parentElement.parentElement.nextElementSibling ) <= -1 && temp == 0){ //add target's parents sibling
     l.push(target.parentElement.parentElement.nextElementSibling);
-    console.log("Being dumb");
   }
-  if(target.parentElement.nextElementSibling != null && l.indexOf(target.parentElement.nextElementSibling) <= -1 && temp == 0){
+  if(target.parentElement.nextElementSibling != null && l.indexOf(target.parentElement.nextElementSibling) <= -1 && temp == 0){ //add targets parents' parent's sibling
     l.push(target.parentElement.nextElementSibling);
   }
   temp = 1;
-  if(l.length == 0 && element.children.length == 0 && element.nextElementSibling == null){
+  if(l.length == 0 && element.children.length == 0 && element.nextElementSibling == null){ //end test
     element.style.border = "thick solid yellow";
     var counter2 = setInterval(function() {
       element.style.border = "";
@@ -46,94 +45,17 @@ function call2(){
     isRunning = false;
     return;
   }
-  if (element.nextElementSibling != null && l.indexOf(element.nextElementSibling) <= -1)
+  if (element.nextElementSibling != null && l.indexOf(element.nextElementSibling) <= -1) //add sibling
     l.push(element.nextElementSibling);
   for (var i = element.children.length - 1; i >= 0; i--) {
     console.log(element.children[i]);
-    if(element.children[i].tagName == "DIV" && l.indexOf(element.children[i]) <= -1){
+    if(element.children[i].tagName == "DIV" && l.indexOf(element.children[i]) <= -1){ //add children
       l.push(element.children[i]);
     }
   }
-  element.style.border = "thick solid yellow";
+  element.style.border = "thick solid yellow"; //change border
   var counter2 = setInterval(function() {
     element.style.border = "";
     clearTimeout(counter2)
   }, 700);
 }
-
-  // function call2() {
-  //   element.style.border = "";
-  //   if(element.children.length > 0){
-  //     for (var i = 0; i < target.children.length; i++) {
-  //       if(element.children[i].tagName == "DIV"){
-  //         element = element.children[i];
-  //         break;
-  //       }
-  //     }
-  //   }
-  //   else if(element.children.length == 0 && element.nextElementSibling != null){
-  //     element = element.nextElementSibling;
-  //     element.style.border = "thick solid yellow";
-  //   }
-  //   else if (element.parentElement.parentElement.nextElementSibling !=null){
-  //     element = element.parentElement.parentElement.nextElementSibling;
-  //     element.style.border = "thick solid yellow";
-  //   }
-  //   element.style.border = "thick solid yellow";
-  //   counter2 = setInterval(function() {
-  //     element.style.border = "";
-  //     clearTimeout(counter2);
-  //   }, 700);
-  //   clearTimeout(counter);
-  //   isRunning = false;
-  //   return;
-  // }
-
-  // var isRunning = false;
-  // var idnum = 0;
-  // function highlight(event){
-  //   for (var i = 0; i < 15; i++) {
-  //     document.getElementById(i).style.border = "";
-  //   }
-  //   var tgtid = event.target.id;
-  //   console.log(idnum);
-  //   console.log(tgtid);
-  //   if(tgtid == "bs-example-navbar-collapse-1")
-  //     tgtid = 0
-  //   else{
-  //     document.getElementById(idnum).style.border = "";
-  //   }
-  //   idnum = tgtid - 1;
-  //   if(isRunning == false){
-  //     console.log(idnum);
-  //     setInterval(call, 700);
-  //     isRunning = true;
-  //   }
-  // }
-  // function call() {
-  //   if (idnum >= 0 && idnum <= 15)
-  //     document.getElementById(idnum).style.border = "";
-  //   if(idnum >= 15){
-  //     console.log("waiting for next click");
-  //   }
-  //   else {
-  //     idnum ++;
-  //     document.getElementById(idnum).style.border = "thick solid yellow";
-  //   }
-  // }
-
-
-
-  // if(target.children.length == 0){
-  //   element = target.nextSibling
-  //   element.style.border = "thick solid yellow";
-  // }
-  // if (target.children.length > 0){
-  //   for (var i = 0; i < target.children.length; i++) {
-  //     if(target.children[i].tagName == "DIV"){
-  //       element = target.children[i]
-  //       element.style.border = "thick solid yellow";
-  //       return;
-  //     }
-  //   }
-  // }
