d69f9a6c67f3c947c340420876a00878a3ef85b8
the-log <2018ankurm@gmail.com>
2017-04-01 21:39:14 -0400

    Server that serves ToC + Lab 3.5

---
+++ web/app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'labs')));
+
+app.get('*', function (req, res) {
+
+  res.sendfile('index.html')
+})
+
+app.listen(8080, function () {
+  console.log('hosted on: http://localhost:8080');
+});
