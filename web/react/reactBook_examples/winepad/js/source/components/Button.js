3af675994fe500b1f627b9dcc6fb9be6ad46ae58
the-log <2018ankurm@gmail.com>
2017-04-04 00:05:43 -0400

    Clearing Old Stuff

---
+++ web/react/reactBook_examples/winepad/js/source/components/Button.js
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+import React, {PropTypes} from 'react';
+
+function Button(props){
+  const cssclasses = classNames('Button', props.className);
+  return props.href
+    ? <a {...props} className={cssclasses}/>
+  : <button {...props} className{cssclassess}/>
+}
+Button.propTypes ={
+  href:PropTypes.string,
+};
+export default Button
