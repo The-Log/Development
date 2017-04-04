16986c81a27a9a1d2172b8d4d70468ed8a05cd88
the-log <2018ankurm@gmail.com>
2017-04-03 22:27:43 -0400

    Moved some files

--- web/app.js
+++ web/app.js
@@ -2,13 +2,12 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'labs')));
+app.use(express.static(path.join(__dirname, 'labs')));//grabs all of my labs folder including CSS and JS
 
-app.get('*', function (req, res) {
-
-  res.sendfile('index.html')
+app.get('/', function (req, res) {
+  res.sendFile('index.html');//sends the top level index file
 })
 
-app.listen(8080, function () {
-  console.log('hosted on: http://localhost:8080');
+app.listen(8080, function () {   //process.env.PORT, process.env.IP //change to this for c9 usage
+  console.log('hosted on: http://localhost:8080'); // host
 });
