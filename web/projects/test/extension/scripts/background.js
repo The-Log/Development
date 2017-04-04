9dc4f408a89fa464f69e020e1e46912df6317767
the-log <2018ankurm@gmail.com>
2017-04-03 22:34:45 -0400

    Moved some files

--- web/projects/test/extension/scripts/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("xd")
-  chrome.tabs.executeScript(null, {
-    file: "replace.js"
-  });
-});
--- web/projects/test/extension/scripts/replace.js
+++
@@ -1,5 +0,0 @@
-var images = document.getElementsByTagName('img'), length = images.length;
-for (var i = 0; i < length; i++) {
-  console.log("lel");
-  images[i].src = "../img/xd_128.png";
-}
