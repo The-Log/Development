fb3fd6051b5ba114388bba02be7f29636c1c94c5
the-log <2018ankurm@gmail.com>
2017-04-26 14:03:55 -0400

    Started Lab05 prototype Chat

---
+++ web/labs/Lab05_Contents/index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var path = require('path');
+var io = require("socket.io")(http);
+var cLog = [];
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", function(socket){
+  // console.log(socket);
+  socket.emit("sendBack", {chatLog:cLog});
+  socket.on("buttonFromClient", function(data){
+    u = data["username"];
+    m = data["message"];
+    s = u + " says: " + m;
+    cLog.push(s);
+    // console.log(cLog);
+    socket.emit("sendBack", {chatLog:cLog});
+  });
+});
+// app.set('port', (process.env.PORT || 8080));
+http.listen(8080, function(){
+  console.log('hosted on: http://localhost:8080');
+})
