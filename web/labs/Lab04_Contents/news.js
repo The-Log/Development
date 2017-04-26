505913b941ea1f9e2f8a1e26aa7b7408d161be18
the-log <2018ankurm@gmail.com>
2017-04-25 22:45:27 -0400

    Finished Lab04 and start Lab05

--- web/labs/Lab04_Contents/news.js
+++ web/labs/Lab04_Contents/news.js
@@ -1,77 +1,121 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var cheerio =  require("cheerio");
 var request = require("request");
 var path = require('path');
 var natural = require("natural");
   TfIdf = natural.TfIdf,
+  nounInflector = new natural.NounInflector();
   tfidf = new TfIdf();
 var pos = require('pos');
 
+var NGrams = natural.NGrams;
+var stopWords = require('stopwords').english;
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-var stopWords = require('stopwords').english;
+var tempURL = "https://www.gutenberg.org/files/1342/1342-h/1342-h.htm";
+var url = tempURL;
+var book = "";
 
-// console.log(stopWords);
+var charsToSig = new Map();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile('index.html');
-})
-
-var tempURL = "http://www.nytimes.com"; //"https://www.gutenberg.org/files/1342/1342-h/1342-h.htm";
-var url = tempURL;
-var book = "";
-var numDocs = 0;
-request(url, function(error, response, html){
-  if(!error && response.statusCode == 200){
-    var $ = cheerio.load(html);
-    $("p").each(function(ix, element) {
-      if (typeof element.children[0] != 'undefined' && typeof element.children[0].data != 'undefined') {
-        var cleanData = element.children[0].data.replace(/(\r\n|\n|\r|\t|    )/gm,"");
-        // console.log(cleanData);
-        cleanData = cleanData.toLowerCase();
-        if (!cleanData.startsWith("by") && cleanData.length > 0) {
-          cleanData = cleanData.replace(/[-—]/g," ");
-          cleanData = cleanData.replace(/[1234567890]/g," ");
-          console.log(cleanData);
-        }
-      }
-    })
-    // $("a").each(function(ix, element) {
-    //   //console.log(element.attribs.href);
-    //   suburl = element.attribs.href
-    //   if (suburl.startsWith("http")) {
-    //     examine(suburl);
-    //     console.log(tfidf);
-    //   }
-    // });
-  }
 });
 
-function examine(url, callback) {
-  var page_contents = ""
-  request(url, function(error, response, html){
-    if(!error && response.statusCode == 200){
-      var $ = cheerio.load(html);
-      $("p").each(function(ix, element) {
-        if (typeof element.children[0] != 'undefined' && typeof element.children[0].data != 'undefined') {
-          var cleanData = element.children[0].data.replace(/(\r\n|\n|\r|\t|    )/gm,"");
-          // console.log(cleanData);
-          cleanData = cleanData.toLowerCase();
-          if (!cleanData.startsWith("by") && cleanData.length > 0) {
-            cleanData = cleanData.replace(/[-—]/g," ");
-            cleanData = cleanData.replace(/[1234567890]/g," ");
-            page_contents += cleanData;
-          }
-        }
-      })
-      callback(page_contents);
-    }
-  });
-}
+app.post('*', function(req,res){
+  console.log(req.body);
+});
 
 app.listen(8080, function () {
   console.log('hosted on: http://localhost:8080');
 });
+
+
+function sendRequest(){
+  request(url, function(error, response, html){
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(html);
+      $("p").each(function(ix, element) {
+        if (typeof element.children[0].data != 'undefined') {
+          var cleanData = element.children[0].data.replace(/(\r\n|\n|\r|\t|    )/gm,"");
+          // console.log(cleanData);
+          if (cleanData.length > 0) {
+             book = book + cleanData;
+          }
+        }
+      })
+
+      book = book.replace(/[.,\/#!?$%\^&\*;:{}=\_`~()’"“”]/g,"");
+      book = book.replace(/[-—]/g," ");
+      // book = book.toLowerCase();
+
+      diagramBook = NGrams.bigrams(book);
+
+      var words = new pos.Lexer().lex(book);
+      var tagger = new pos.Tagger();
+      var taggedWords = tagger.tag(words);
+
+      importantWords = "";
+
+      for (var i = 0; i < taggedWords.length - 1; i++) {
+        var taggedWord1 = taggedWords[i];
+        var word1 = taggedWord1[0];
+        var tag1 = taggedWord1[1];
+        var taggedWord2 = taggedWords[i+1];
+        var word2 = taggedWord2[0];
+        var tag2 = taggedWord2[1];
+        word1 = nounInflector.singularize(word1)
+        word2 = nounInflector.singularize(word2)
+        if(word1.startsWith('Mr') || word1.startsWith('Miss') && tag2 == "NNP"){
+          word1 = word1 +". "+ word2;
+        }
+        if(tag1 == 'NNP' && word1 != "Ill"){
+          if (charsToSig.has("Mr. " + word1)) {
+            word1 = "Mr. "+ word1;
+          }
+          if (charsToSig.has("Mrs. " + word1)) {
+            word1 = "Mrs. "+ word2;
+          }
+          if(charsToSig.has(word1)){
+            var freq = charsToSig.get(word1) + 1;
+            charsToSig.set(word1, freq);
+          }
+          else{
+            var freq = 1;
+            charsToSig.set(word1, freq)
+          }
+        }
+      }
+
+      a = [];
+      for(var x of charsToSig)
+        a.push(x);
+
+      a.sort(function(x, y) {
+        return y[1] - x[1];
+      });
+
+      // console.log(a);
+      characters = "";
+      for (var i = 0; i < 5; i++) {
+        if (i < 4) {
+          characters += a[i][0] + ", "
+        }
+        else {
+          characters += "and " + a[i][0] + "."
+        }
+      }
+
+      console.log("The 5 most significant/locations characters are: " + characters);
+
+      app.get('/', function (req, res) {
+        res.write("The 5 most significant/locations characters are: " + characters);
+        res.end();
+      });
+      trigramBook = NGrams.trigrams(book);
+    }
+  });
+}
